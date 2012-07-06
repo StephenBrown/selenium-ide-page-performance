aacea11f2fa9079e258d5bd4cb43e9916ae123df
Stephen Brown <browns.store@gmail.com>
2012-07-06 04:27:19 -0600

    Add use of a beacon id

--- page-speed-commands.js
+++ page-speed-commands.js
@@ -1,6 +1,19 @@
-Selenium.prototype.doRunYslow = function(){
+Selenium.prototype.doRunYslow = function(beaconId){
 	var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);
 	var browserWindow = wm.getMostRecentWindow("navigator:browser");
 
+	if (beaconId) {
+		var beaconUrl = browserWindow.YSLOW.util.Preference.getPref('beaconUrl');
+		var beaconUrlWithPageId = beaconUrl;
+		beaconUrlWithPageId = beaconUrl + beaconId + '/';
+		browserWindow.YSLOW.util.Preference.setPref('beaconUrl', beaconUrlWithPageId);
+	}
+
 	browserWindow.YSLOW.firefox.run(false);
+
+	if (beaconId) {
+		browserWindow.YSLOW.util.event.addListener('onUnload', function() {
+			browserWindow.YSLOW.util.Preference.setPref('beaconUrl', beaconUrl);
+		});
+	}
 };
